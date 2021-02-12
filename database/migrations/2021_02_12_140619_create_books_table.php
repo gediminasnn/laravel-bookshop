063c9a0d0ef3b5c8f9061b29f0d3382f432cce78
gediminasnn <neniusgediminas@gmail.com>
2021-02-12 16:51:12 +0200

    Readded migrations

--- database/migrations/2021_02_12_140619_create_books_table.php
+++ database/migrations/2021_02_12_140619_create_books_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBookAuthorTable extends Migration
+class CreateBooksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateBookAuthorTable extends Migration
      */
     public function up()
     {
-        Schema::create('book_author', function (Blueprint $table) {
+        Schema::create('books', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreateBookAuthorTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('book_author');
+        Schema::dropIfExists('books');
     }
 }
--- database/migrations/2021_02_12_140645_create_book_author_table.php
+++ database/migrations/2021_02_12_140645_create_book_author_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBookTable extends Migration
+class CreateBookAuthorTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,9 @@ class CreateBookTable extends Migration
      */
     public function up()
     {
-        Schema::create('book', function (Blueprint $table) {
-            $table->id();
-            $table->string('title');
-//            $table->string('author');
-            $table->string('cover');
-//            $table->string('genre');
-            $table->timestamps();
+        Schema::create('book_author', function (Blueprint $table) {
+            $table->foreignId('book_id')->constrained();
+            $table->foreignId('author_id')->constrained();
         });
     }
 
@@ -30,6 +26,6 @@ class CreateBookTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('book');
+        Schema::dropIfExists('book_author');
     }
 }
