024aa1411dc2894e60ae2d8aed28071647a6e15d
gediminasnn <neniusgediminas@gmail.com>
2021-02-12 16:51:03 +0200

    Created first working controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>tp_projektas</title>
+</head>
+
+<body>
+Hello World!
+@foreach ($users as $user)
+    <p>This is user {{ $user->id }}</p>
+@endforeach
+</body>
+</html>
