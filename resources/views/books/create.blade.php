967900369722eb2404462256633998fd6730482f
gediminasnn <neniusgediminas@gmail.com>
2021-02-19 10:58:06 +0200

    finished book.create form

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -10,55 +10,76 @@
             <form action="#" method="POST">
                 <div class="overflow-hidden shadow rounded-md">
                     <div class="px-4 py-5 bg-white sm:p-6">
-                        <div class="grid grid-cols-6 gap-6">
+                        <div class="grid grid-cols-4 gap-6">
+
                             <div class="col-span-6 sm:col-span-3">
-                                <label for="first_name" class="block text-sm font-medium text-gray-700">First name</label>
+                                <label for="first_name" class="block text-sm font-medium text-gray-700">Title</label>
                                 <input type="text" name="first_name" id="first_name" autocomplete="given-name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                             </div>
 
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="last_name" class="block text-sm font-medium text-gray-700">Last name</label>
-                                <input type="text" name="last_name" id="last_name" autocomplete="family-name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <div class="col-span-6 sm:row-start-2 sm:col-span-2">
+                                <label for="first_name" class="block text-sm font-medium text-gray-700">Price</label>
+                                <input placeholder="$" type="text" name="first_name" id="first_name" autocomplete="given-name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                             </div>
 
-                            <div class="col-span-6 sm:col-span-4">
-                                <label for="email_address" class="block text-sm font-medium text-gray-700">Email address</label>
-                                <input type="text" name="email_address" id="email_address" autocomplete="email" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <div class="col-span-6 sm:row-start-2 sm:col-span-2">
+                                <label for="first_name" class="block text-sm font-medium text-gray-700">Discount</label>
+                                <input placeholder="%" type="text" name="first_name" id="first_name" autocomplete="given-name" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                             </div>
 
-                            <div class="col-span-6 sm:col-span-3">
-                                <label for="country" class="block text-sm font-medium text-gray-700">Country / Region</label>
+                            <div class="col-span-6 sm:row-start-3 sm:col-span-3">
+                                <label for="country" class="block text-sm font-medium text-gray-700">Author</label>
                                 <select id="country" name="country" autocomplete="country" class="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                                    <option>United States</option>
+                                    <option>Zac Muckerberg</option>
                                     <option>Canada</option>
                                     <option>Mexico</option>
                                 </select>
                             </div>
 
-                            <div class="col-span-6">
-                                <label for="street_address" class="block text-sm font-medium text-gray-700">Street address</label>
-                                <input type="text" name="street_address" id="street_address" autocomplete="street-address" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <div class="col-span-6 sm:row-start-4 sm:col-span-3">
+                                <label for="country" class="block text-sm font-medium text-gray-700">Genre</label>
+                                <select id="country" name="country" autocomplete="country" class="block w-full px-3 py-2 mt-1 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    <option>Drama</option>
+                                    <option>Canada</option>
+                                    <option>Mexico</option>
+                                </select>
                             </div>
 
-                            <div class="col-span-6 sm:col-span-6 lg:col-span-2">
-                                <label for="city" class="block text-sm font-medium text-gray-700">City</label>
-                                <input type="text" name="city" id="city" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <div class="col-span-6 sm:col-span-4">
+                                <label for="email_address" class="block text-sm font-medium text-gray-700">Description</label>
+                                <textarea name="description" rows="5" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
                             </div>
 
-                            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                <label for="state" class="block text-sm font-medium text-gray-700">State / Province</label>
-                                <input type="text" name="state" id="state" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <div class="col-span-6 sm:col-span-4">
+                                <label class="block text-sm font-medium text-gray-700">
+                                    Cover photo
+                                </label>
+                                <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                                    <div class="space-y-1 text-center">
+                                        <svg class="mx-auto h-12 w-12 text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
+                                            <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                        </svg>
+                                        <div class="flex text-sm text-gray-600">
+                                            <label for="file-upload" class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                                <span>Upload a file</span>
+                                                <input id="file-upload" name="file-upload" type="file" class="sr-only">
+                                            </label>
+                                            <p class="pl-1">or drag and drop</p>
+                                        </div>
+                                        <p class="text-xs text-gray-500">
+                                            PNG, JPG, GIF up to 10MB
+                                        </p>
+                                    </div>
+                                </div>
                             </div>
 
-                            <div class="col-span-6 sm:col-span-3 lg:col-span-2">
-                                <label for="postal_code" class="block text-sm font-medium text-gray-700">ZIP / Postal</label>
-                                <input type="text" name="postal_code" id="postal_code" autocomplete="postal-code" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                            </div>
+
+
                         </div>
                     </div>
                     <div class="px-4 py-3 text-right bg-gray-50 sm:px-6">
                         <button type="submit" class="inline-flex justify-center px-4 py-2 text-sm font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                            Save
+                            Add
                         </button>
                     </div>
                 </div>
