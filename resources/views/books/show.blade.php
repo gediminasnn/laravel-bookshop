805cf2f97b4de843e4ce67d5338409184bc68672
gediminasnn <neniusgediminas@gmail.com>
2021-02-22 20:48:15 +0200

    book page front-end completed, half back-end completed

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -1,53 +1,101 @@
 <x-app-layout>
+
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            Book details
+        <h2 class="text-xl font-semibold leading-tight text-gray-800">
+            Book listing
         </h2>
     </x-slot>
 
-    <div class="control-group rounded">
+    </body>
+    <div class="py-6">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
+                <div class="grid grid-cols-2 md:grid-cols-6">
 
-        <div class=""></div>
-        <div class="row">
-            <div class="col m-auto">
-                <a href="">
-                    <img class="rounded mx-auto d-block" src="{{URL::asset('/images/IMG_20210206_152320.jpg')}}" alt="" style="height: 400px">
-                </a>
-            </div>
-            <div class="col-lg-9">
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item">Title : Hakingas</li>
-                        <li class="list-group-item">Author : Stephen Hawking</li>
-                        <li class="list-group-item">Price : 420.0$</li>
-                        <li class="list-group-item">
-                            Rating :
-                            <button type="button" class="btn btn-light">1</button>
-                            <button type="button" class="btn btn-light">2</button>
-                            <button type="button" class="btn btn-light">3</button>
-                            <button type="button" class="btn btn-warning">4</button>
-                            <button type="button" class="btn btn-light">5</button>
-                        </li>
-                        <li class="list-group-item">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</li>
-                    </ul>
+                    <div class="col-span-2 p-10 bg-white border-b border-gray-200">
+                            <img class="rounded mx-auto d-block" src="{{URL::asset("/images/".$book->cover)}}" alt="" >
+                    </div>
+
+                    <div class="col-span-4 p-10 bg-white border-b border-gray-200">
+
+                        <label for="first_name" class="block text-4xl font-medium text-gray-700">{{$book->title}}</label>
+                        <hr>
+                        <p>
+                            <span class="text-2xl font-medium text-gray-700">Author</span>
+                            <span class="class=inline-block px-2 font-medium text-blue-700 transition bg-transparent border-2 border-blue-700 rounded-full shadow ripple focus:outline-none">Jon Bomnori</span>
+                            <span class="class=inline-block px-2 font-medium text-blue-700 transition bg-transparent border-2 border-blue-700 rounded-full shadow ripple focus:outline-none">Jon Bomnori</span>
+                            <span class="class=inline-block px-2 font-medium text-blue-700 transition bg-transparent border-2 border-blue-700 rounded-full shadow ripple focus:outline-none">Jon Bomnori</span>
+                        </p>
+
+
+                        <p>
+                            <span class="text-xl font-medium text-gray-700">Genre</span>
+                            <span class="class=inline-block text-xs px-2 font-medium text-blue-700 transition bg-transparent border-2 border-blue-700 rounded-full shadow ripple focus:outline-none">Jon Bomnori</span>
+                            <span class="class=inline-block text-xs px-2 font-medium text-blue-700 transition bg-transparent border-2 border-blue-700 rounded-full shadow ripple focus:outline-none">Jon Bomnori</span>
+                            <span class="class=inline-block text-xs px-2 font-medium text-blue-700 transition bg-transparent border-2 border-blue-700 rounded-full shadow ripple focus:outline-none">Jon Bomnori</span>
+                        </p>
+
+                        <p><span class="font-medium text-gray-700">Rating : 4.7/5</span></p>
+
+                        <p class="pt-10 pr-5 text-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    </div>
+
+                </div>
+
+
+                <div class="p-10 bg-white border-b border-gray-200">
+                    <label for="first_name" class="block text-3xl font-medium text-gray-700">Reviews</label>
+                    <hr>
+
+                    <p class="py-2">
+                        <span class="text-xl px-2 border-gray-700 rounded-full shadow ripple border-2">Tom Hardy</span>
+                    </p>
+                    <p class="pr-5 pb-2 text-xl">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                    <hr>
+
+
+                    <!-- Comment section -->
+                    <div class="col-span-6 sm:col-span-4">
+                        <form method="POST" action="#">
+                        @csrf
+
+
+                                <x-label class="text-2xl" for="comment" :value="__('Comment')" />
+
+                                <div class="flex inline-flex items-center">
+
+                                    <input type="radio" class="form-radio" name="rating" value="1">
+                                    <span class="mx-1">1</span>
+
+                                    <input type="radio" class="form-radio" name="rating" value="2">
+                                    <span class="mx-1">2</span>
+
+                                    <input type="radio" class="form-radio" name="rating" value="3">
+                                    <span class="mx-1">3</span>
+
+                                    <input type="radio" class="form-radio" name="rating" value="4">
+                                    <span class="mx-1">4</span>
+
+                                    <input type="radio" class="form-radio" name="rating" value="5">
+                                    <span class="mx-1">5</span>
+
+                                </div>
+                                <textarea name="comment" rows="5" class="block w-full mt-1 border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"></textarea>
+
+                            <div class="flex justify-end mt-4">
+                                <x-button>
+                                    {{ __('Submit') }}
+                                </x-button>
+                            </div>
+                        </form>
+                    </div>
+
+                </div>
+
+
             </div>
         </div>
-
-        <div class="row mt-1">
-            <div class="col-lg-12">
-                <ul class="list-group list-group-flush">
-                    <b><li class="list-group-item">Write a review</li></b>
-                    <li class="list-group-item">John Doe : Lorem Ipsum is simply dummy text of the printing and typesetting</li>
-                    <li class="list-group-item">John Doe : Lorem Ipsum is simply dummy text of the printing and typesetting</li>
-                    <li class="list-group-item">John Doe : Lorem Ipsum is simply dummy text of the printing and typesetting</li>
-                </ul>
-            </div>
-            <div class="form-group">
-                <label for="exampleFormControlTextarea1">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-            </div>
-
-        </div>
-        <button type="button" class="btn btn-primary">Primary</button>
+    </div>
 
     </div>
 </x-app-layout>
