8ee51a859a95e64a7f48d6aff1fe99b135285e01
gediminasnn <neniusgediminas@gmail.com>
2021-02-22 20:47:09 +0200

    half work done with dashboard frontend

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -8,11 +8,18 @@
 
 
 </body>
-    <div class="py-12">
+    <div class="py-6">
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
-            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+            <div class="grid grid-cols-4 overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div class="col-span-1 border-r">
+                    <a href="#" class="block px-10 py-5 border-b text-xl hover:bg-blue-50 @if(request()->routeIs('books')) bg-blue-50 @endif">My Books</a>
+                    <a href="#" class="block px-10 py-5 border-b text-xl hover:bg-blue-50 @if(request()->routeIs('books')) bg-blue-50 @endif">Add a Book</a>
+                    <a href="#" class="block px-10 py-5 border-b text-xl hover:bg-blue-50 @if(request()->routeIs('books')) bg-blue-50 @endif">My Comments</a>
+                    <a href="#" class="block px-10 py-5 border-b text-xl hover:bg-blue-50 @if(request()->routeIs('books')) bg-blue-50 @endif">Report a Book</a>
+                    <a href="#" class="block px-10 py-5 border-b text-xl hover:bg-blue-50 @if(request()->routeIs('books')) bg-blue-50 @endif">Change Password</a>
+                </div>
+                <div class="col-span-3 px-10 py-3">
+                    <a href="#" class="">test</a>
                 </div>
             </div>
         </div>
