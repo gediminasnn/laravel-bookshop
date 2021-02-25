07756ee13755b52df0950a2ea5868e52d8b4f2e4
gediminasnn <neniusgediminas@gmail.com>
2021-02-26 00:58:12 +0200

    created user() and book() relations to review model

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -10,4 +10,14 @@ class Review extends Model
     use HasFactory;
 
     protected $fillable = ['book_id', 'user_id', 'stars', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
 }
