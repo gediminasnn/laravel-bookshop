c8b4122b53f0a7126f93276e3a6ce3f68b3d5d1a
gediminasnn <neniusgediminas@gmail.com>
2021-02-26 01:06:08 +0200

    created and finished ReviewController.php

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Review;
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        Review::create([
+            'book_id' => $request->get('id'),
+            'user_id' => auth()->user()->id,
+            'stars' => $request->star,
+            'comment' => $request->comment
+        ]);
+
+        return redirect()->route('books.show',$request->get('id'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $review = Review::find($id);
+        return response()->view('reviews.edit', ['review' => $review]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $review = Review::find($id);
+
+
+
+        $review->update([
+            'stars' => $request->star,
+            'comment' => $request->comment
+        ]);
+        return redirect()->route('books.show',['id' => $review->book_id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Review  $review
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        Review::find($id)->delete();
+
+        return redirect()->back();
+    }
+}
