bd3528e81239724745d395159dd948598db30368
gediminasnn <neniusgediminas@gmail.com>
2021-02-27 00:13:08 +0200

    fixed dashboard report book route

--- resources/views/components/dashboard.blade.php
+++ resources/views/components/dashboard.blade.php
@@ -16,7 +16,7 @@
                     <a href="{{route('books.create')}}" class="block px-10 py-5 border-b hover:bg-blue-50 @if(request()->routeIs('dashboard.add-a-book')) bg-blue-50 @endif">Add a Book</a>
                     <a href="{{route('dashboard.my-reviews')}}" class="block px-10 py-5 border-b hover:bg-blue-50 @if(request()->routeIs('dashboard.my-reviews')) bg-blue-50 @endif">My Reviews</a>
                     <a href="{{route('dashboard.my-reports')}}" class="block px-10 py-5 border-b hover:bg-blue-50 @if(request()->routeIs('dashboard.my-reports')) bg-blue-50 @endif">My Reports</a>
-                    <a href="{{route('dashboard.report-a-book')}}" class="block px-10 py-5 border-b hover:bg-blue-50 @if(request()->routeIs('dashboard.report-a-book')) bg-blue-50 @endif">Report a Book</a>
+                    <a href="{{route('reports.create')}}" class="block px-10 py-5 border-b hover:bg-blue-50 ">Report a Book</a>
                     <a href="{{route('dashboard.change-email')}}" class="block px-10 py-5 border-b hover:bg-blue-50 @if(request()->routeIs('dashboard.change-email')) bg-blue-50 @endif">Change email</a>
                     <a href="{{route('dashboard.change-password')}}" class="block px-10 py-5 border-b hover:bg-blue-50 @if(request()->routeIs('dashboard.change-password')) bg-blue-50 @endif">Change Password</a>
                     @if(Auth::user()->is_admin)
