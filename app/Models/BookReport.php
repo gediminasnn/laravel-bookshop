bbf0850c9560182f0bac57d7cf106cfe53fd1eef
gediminasnn <neniusgediminas@gmail.com>
2021-02-26 22:07:17 +0200

    created user relation for BookReport.php

--- app/Models/BookReport.php
+++ app/Models/BookReport.php
@@ -15,4 +15,9 @@ class BookReport extends Model
     {
         return $this->belongsTo(Book::class);
     }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
