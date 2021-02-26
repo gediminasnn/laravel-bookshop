f4fff8b1a5a03333f585fa062a2fc414260df4e0
gediminasnn <neniusgediminas@gmail.com>
2021-02-26 13:04:55 +0200

    fixed show.blade.php so guest can enter it

--- resources/views/books/show.blade.php
+++ resources/views/books/show.blade.php
@@ -13,28 +13,28 @@
             <div class="bg-white overflow-hidden shadow-md sm:rounded-lg">
 
                 <div class="grid grid-cols-2 md:grid-cols-6">
-
-                    @if($book->user_id === auth()->user()->id)
-                    <div class="col-span2 md:col-span-6">
-                        <div class="flex justify-start space-x-1 col-span-2 md:col-span-6 p-5 bg-white border-b border-gray-200">
-                            <x-a href="{{route('dashboard')}}">Dashboard</x-a>
-                            <a href="{{route("books.edit", $book->id)}}" class="border-2 border-indigo-200 rounded-md p-2">
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                </svg>
-                            </a>
-                            <form method="POST" action="{{route("books.destroy", $book->id)}}">
-                                <button type="submit" class="border-2 border-red-200 rounded-md p-2">
-                                    @csrf
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-red-500">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                    @if(!Auth::guest())
+                        @if($book->user_id == auth()->user()->id)
+                        <div class="col-span2 md:col-span-6">
+                            <div class="flex justify-start space-x-1 col-span-2 md:col-span-6 p-5 bg-white border-b border-gray-200">
+                                <x-a href="{{route('dashboard')}}">Dashboard</x-a>
+                                <a href="{{route("books.edit", $book->id)}}" class="border-2 border-indigo-200 rounded-md p-2">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-500">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                     </svg>
-                                </button>
-                            </form>
+                                </a>
+                                <form method="POST" action="{{route("books.destroy", $book->id)}}">
+                                    <button type="submit" class="border-2 border-red-200 rounded-md p-2">
+                                        @csrf
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-red-500">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                    </button>
+                                </form>
+                            </div>
                         </div>
-                    </div>
+                        @endif
                     @endif
-
 
                     <div class="col-span-2 p-10 bg-white border-b border-gray-200">
                             <img class="rounded mx-auto d-block" src="{{URL::asset("/images/".$book->cover)}}" alt="" >
@@ -80,24 +80,24 @@
                         <p class="py-2">
                             <span class="text-xl px-2 border-gray-700 rounded-full shadow ripple border-2">{{$review->user->name}}</span>
                             Stars : {{$review->stars}}
-
-                        @if($review->user_id === auth()->user()->id)
-
-                            <div class="flex justify-start space-x-1">
-                                <a href="{{route("reviews.edit", $review->id)}}" class="border-2 border-indigo-200 rounded-md p-1">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-500">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
-                                    </svg>
-                                </a>
-                                <form method="POST" action="{{route("reviews.destroy", $review->id)}}">
-                                    <button type="submit" class="border-2 border-red-200 rounded-md p-1">
-                                        @csrf
-                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-red-500">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                        @if(!Auth::guest())
+                            @if($review->user_id === auth()->user()->id)
+                                <div class="flex justify-start space-x-1">
+                                    <a href="{{route("reviews.edit", $review->id)}}" class="border-2 border-indigo-200 rounded-md p-1">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-gray-500">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" />
                                         </svg>
-                                    </button>
-                                </form>
-                            </div>
+                                    </a>
+                                    <form method="POST" action="{{route("reviews.destroy", $review->id)}}">
+                                        <button type="submit" class="border-2 border-red-200 rounded-md p-1">
+                                            @csrf
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-4 w-4 text-red-500">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
+                                        </button>
+                                    </form>
+                                </div>
+                            @endif
                         @endif
                         </p>
                         <p class="pr-5 pb-2 text-xl">{{$review->comment}}</p>
