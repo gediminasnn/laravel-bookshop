72da777e501a18a25105d0280f0ff4e33443f0e2
gediminasnn <neniusgediminas@gmail.com>
2021-02-27 00:11:04 +0200

    added status attribute with default 1

--- app/Models/BookReport.php
+++ app/Models/BookReport.php
@@ -9,7 +9,9 @@ class BookReport extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['user_id', 'book_id', 'report'];
+    protected $fillable = ['user_id', 'book_id', 'report', 'status'];
+
+    protected $attributes = ['status' => 1];
 
     public function book()
     {
