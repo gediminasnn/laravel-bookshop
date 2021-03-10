0e2d3c99f419d13b3fa4df0c898cfc0d50fd49f9
gediminasnn <neniusgediminas@gmail.com>
2021-03-10 21:58:47 +0200

    Added working v1/books apiResource route to api.php

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('v1/books', BookApiController::class);
+
 Route::middleware('auth:api')->get('/dashboard', function (Request $request) {
     return $request->user();
 });
+
+
+
+
